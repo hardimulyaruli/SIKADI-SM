a7c56290596508637eb6bb4857c71f6b9167e79f
ashaury <herdi.ashaury@lecture.unjani.ac.id>
2025-11-22 13:20:42 +0700

    Perubahan Dashboard Keuangan

---
+++ resources/views/keuangan/gaji.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.sidebar')
+
+@section('content')
+<h2>💰 Gaji Pegawai</h2>
+<hr>
+
+<p>Halaman gaji pegawai (kosong).</p>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/owner/dashboard', function () {
     return view('dashboard.owner');
 })->name('owner.dashboard');
 
-//keuangan
 Route::get('/keuangan/dashboard', function () {
     return view('dashboard.keuangan');
 })->name('keuangan.dashboard');
@@ -36,3 +35,23 @@ Route::get('/owner/manajemen-pengguna', function () {
     return view('owner.user-management');
 })->name('owner.user_management');
 
+// MENU KEUANGAN
+Route::get('/keuangan/pemasukan', function () {
+    return view('keuangan.pemasukan');
+})->name('keuangan.pemasukan');
+
+Route::get('/keuangan/pengeluaran', function () {
+    return view('keuangan.pengeluaran');
+})->name('keuangan.pengeluaran');
+
+Route::get('/keuangan/gaji-pegawai', function () {
+    return view('keuangan.gaji');
+})->name('keuangan.gaji');
+
+Route::get('/keuangan/pinjaman', function () {
+    return view('keuangan.pinjaman');
+})->name('keuangan.pinjaman');
+
+Route::get('/keuangan/laporan', function () {
+    return view('keuangan.laporan');
+})->name('keuangan.laporan');
